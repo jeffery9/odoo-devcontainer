76061ac408deb12c244658a17acd608d4b207b20
jeffery <jeffery9@outlook.com>
2024-10-05 20:21:44 +0800

    fix

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget https://nightly.odoo.com/18.0/nightly/src/odoo_18.0.latest.zip
-unzip odoo_18.0.latest.zip -d docker/odoo18
+unzip -oqq odoo_18.0.latest.zip  -d docker/ && mv docker/odoo-18*  docker/odoo18
